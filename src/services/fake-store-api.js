15c77f0244aeb31b712ddd5f8444cca6570dac90
Nilanchala Bariki <98546192+Nilugithubbariki@users.noreply.github.com>
2023-09-03 14:14:29 +0530

    Search function

--- src/services/fake-store-api.js
+++ src/services/fake-store-api.js
@@ -11,8 +11,8 @@ const FakeStoreApi = {
     },
     fetchProductsBySearchQuery: async (query) => {
         const res = await fetch("https://fakestoreapi.com/products")
-        const result = await res.json()
-        return result.filter((product) => product.title.toLowerCase().includes(query))
+      const result = await res.json()
+        return result.filter((product) => product.title.toUpperCase().includes(query.toUpperCase()))
     },
 }
 
